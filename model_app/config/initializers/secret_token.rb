e9911d770e20a2b3a4e4d80069d9ec7349366bbc
yopming <108957931@qq.com>
2011-03-15 15:20:20 +0800

    315 commit

---
+++ model_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ModelApp::Application.config.secret_token = '5eb496057eaae3b3f0916fb93d74a70e36437f9bcd7c3ff0baa206193f860750be1f207aff718ef8c47c5287d4cf66e18d5f240f0f736e19e424e3303b3899a3'
